53e25d74e24ad6abc6ccb5db3d12efcafe19ccaf
khansuhl <khansuhl@gmail.com>
2019-02-06 16:59:04 +0530

    trying to copy file from s3 to ec2 instance- trial 1

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -3,3 +3,7 @@ sudo apt-get update
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo apt install python-pip -y
+export LC_ALL=en_US.UTF-8
+pip install awscli --upgrade --user
+pip install --upgrade pip
